48e532b2bc702a3e888712ec39312499d4292173
jejeremylee <myjejeremy@gmail.com>
2020-10-04 22:54:03 +0700

    Lab 03

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/jeremy/lab03/MainActivity.kt
@@ -0,0 +1,123 @@
+package id.ac.ui.cs.mobileprogramming.jeremy.lab03
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.os.Handler
+import android.view.KeyEvent
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+    private var backPressedTime = 0L
+    private var seconds = 0
+    private var running = false
+    private var wasRunning = false
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            exitByBackKey()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+    protected fun exitByBackKey() {
+        val alertbox: AlertDialog = AlertDialog.Builder(this)
+            .setMessage("Do you want to exit application?")
+            .setPositiveButton("Yes", DialogInterface.OnClickListener { arg0, arg1 ->
+
+                finish()
+            })
+            .setNegativeButton("No", DialogInterface.OnClickListener { arg0, arg1 -> })
+            .show()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState != null) {
+
+            seconds = savedInstanceState
+                .getInt("seconds")
+            running = savedInstanceState
+                .getBoolean("running")
+            wasRunning = savedInstanceState
+                .getBoolean("wasRunning")
+        }
+        runTimer()
+    }
+    override fun onSaveInstanceState(
+        savedInstanceState: Bundle
+    ) {
+        savedInstanceState
+            .putInt("seconds", seconds)
+        savedInstanceState
+            .putBoolean("running", running)
+        savedInstanceState
+            .putBoolean("wasRunning", wasRunning)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        wasRunning = running
+        running = true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (wasRunning) {
+            running = true
+        }
+    }
+
+    fun onClickStart(view: View?) {
+        running = true
+    }
+
+    fun onClickStop(view: View?) {
+        running = false
+        val timeView = findViewById(R.id.start_button) as TextView
+        timeView.text = "RESUME"
+    }
+
+
+    fun onClickReset(view: View?) {
+        running = false
+        seconds = 0
+        val timeView = findViewById(R.id.start_button) as TextView
+        timeView.text = "START"
+    }
+
+    private fun runTimer() {
+
+        val timeView = findViewById(R.id.time_view) as TextView
+
+        val handler = Handler()
+
+        handler.post(object : Runnable {
+            override fun run() {
+                val hours = seconds / 3600
+                val minutes = seconds % 3600 / 60
+                val secs = seconds % 60
+
+                val time: String = java.lang.String
+                    .format(
+                        Locale.getDefault(),
+                        "%d:%02d:%02d", hours,
+                        minutes, secs
+                    )
+
+                timeView.text = time
+
+                if (running) {
+                    seconds++
+                }
+
+                handler.postDelayed(this, 1000)
+            }
+        })
+    }
+}
